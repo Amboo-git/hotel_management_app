2ab8fade7ed528a48a76537120d90350958002d1
Amboo-git <polaris.x.2005@gmail.com>
2026-01-04 14:19:23 +0800

    增强缓存逻辑可读性

--- src/main/java/me/amboo/algorithm/WeightGenerator.java
+++ src/main/java/me/amboo/algorithm/WeightGenerator.java
@@ -11,11 +11,12 @@ public class WeightGenerator {
     public static int getWeight(int roomID1, int roomID2) {
         String key = roomID1 + "-" + roomID2;
 
-        // 如果缓存中已经存在该边的权值，则直接返回，不再生成新的
-        return weightCache.computeIfAbsent(key, k -> 
-            ThreadLocalRandom.current().nextInt(-20, 41)
-            // nextInt(min, max) 的范围是 [-20, 41)
-        );
+        // 如果缓存中不存在权值，生成新权值并缓存；否则直接返回
+        if (!weightCache.containsKey(key)) {
+            int weight = ThreadLocalRandom.current().nextInt(-20, 41);
+            weightCache.put(key, weight);
+        }
+        return weightCache.get(key);
     }
 
     // 提供一个清除缓存的方法，方便管理员在需要时重新随机化整个酒店的图结构
